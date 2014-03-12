d9c7cce05f183eae05ec1f687799e17322a82387
Sho Kusano <rosylilly@aduca.org>
2014-03-12 17:19:41 +0900

    Fix type miss
    - raise_errors_for_deprecations -> raise_errors_for_deprecations!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "response_code_matchers"
 
 RSpec.configure do |config|
-  if config.respond_to?(:raise_errors_for_deprecations)
+  if config.respond_to?(:raise_errors_for_deprecations!)
     config.raise_errors_for_deprecations!
   end
   config.run_all_when_everything_filtered = true
